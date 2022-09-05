970e406fb600df8e94032d84e4cddd5e86a48bcf
Franco Vega <franco.vegam@gmail.com>
2022-09-06 10:45:29 +1200

    styling code pep8

--- src/battleship.py
+++ src/battleship.py
@@ -1,59 +1,59 @@
 import random
 from src.constants import NUMBER_SHIPS
 
-          
+
 class Battleship:
-  def __init__(self, board):
-    self.board = board
+    def __init__(self, board):
+        self.board = board
 
-  def create_ships(self):
-    self.ships = []
-    for i in range(NUMBER_SHIPS):
-      self.x_row, self.y_column = random.randint(0, 7), random.randint(0, 7)
-      while self.board[self.x_row][self.y_column] == "X":
-        self.x_row, self.y_column = random.randint(0, 7), random.randint(0, 7)
-      self.board[self.x_row][self.y_column] = "X"
-      self.ships.append([self.x_row,self.y_column])
-    return self.board, self.ships
+    def create_ships(self):
+        self.ships = []
+        for i in range(NUMBER_SHIPS):
+            self.x_row, self.y_column = random.randint(
+                0, 7), random.randint(0, 7)
+            while self.board[self.x_row][self.y_column] == "X":
+                self.x_row, self.y_column = random.randint(
+                    0, 7), random.randint(0, 7)
+            self.board[self.x_row][self.y_column] = "X"
+            self.ships.append([self.x_row, self.y_column])
+        return self.board, self.ships
+
+    def get_user_input(self):
+        try:
+            x_row = input("Enter the row of the ship: ")
+            while x_row not in '12345678' or int(x_row) > 10:
+                print('Not an appropriate choice, please select a valid row')
+                x_row = input("Enter the row of the ship: ")
+
+            y_column = input("Enter the column of the ship: ")
+            while y_column not in "12345678" or int(y_column) > 10:
+                print('Not an appropriate choice, please select a valid column')
+                y_column = input("Enter the column of the ship: ")
+            return int(x_row)-1, int(y_column)-1
+        except ValueError and KeyError:
+            print("Not a valid input")
+            return self.get_user_input()
+
+    def count_hit_ships(self):
+        hit_ships = 0
+        for row in self.board:
+            for column in row:
+                if column == "X":
+                    hit_ships += 1
+        return hit_ships
 
 
-  def get_user_input(self):
-    try:
-      x_row= input("Enter the row of the ship: ")
-      while x_row not in '12345678' or int(x_row) > 10:
-          print('Not an appropriate choice, please select a valid row')
-          x_row = input("Enter the row of the ship: ")
-
-      y_column = input("Enter the column of the ship: ")
-      while y_column not in "12345678" or int(y_column) > 10:
-          print('Not an appropriate choice, please select a valid column')
-          y_column = input("Enter the column of the ship: ")
-      return int(x_row)-1, int(y_column)-1
-    except ValueError and KeyError:
-      print("Not a valid input")
-      return self.get_user_input()
-    
-  
-  def count_hit_ships(self):
-    hit_ships = 0
-    for row in self.board:
-      for column in row:
-        if column == "X":
-          hit_ships += 1
-    return hit_ships
-
-def create_message(x,y,ships):
+def create_message(x, y, ships):
     message = []
     for i in range(len(ships)):
-        x_guess,y_guess = x,y
-        if (abs(x_guess-ships[i][0])+
-              abs(y_guess-ships[i][1])) <= 2:
-            message.append("SHIP_{0}:HOT".format(i+1))    
+        x_guess, y_guess = x, y
+        if (abs(x_guess-ships[i][0]) +
+                abs(y_guess-ships[i][1])) <= 2:
+            message.append("SHIP_{0}:HOT".format(i+1))
         else:
-            if (abs(x_guess-ships[i][0])+
-              abs(y_guess-ships[i][1])) <= 4:
+            if (abs(x_guess-ships[i][0]) +
+                    abs(y_guess-ships[i][1])) <= 4:
                 message.append("SHIP_{0}:WARM".format(i+1))
             else:
                 message.append("SHIP_{0}:COLD".format(i+1))
     return message
-
--- src/main.py
+++ src/main.py
@@ -1,45 +1,48 @@
 
-from src import Battleship,create_message
-from src import BOARD_SIZE,NUMBER_GUESS,NUMBER_SHIPS
+from src import Battleship, create_message
+from src import BOARD_SIZE, NUMBER_GUESS, NUMBER_SHIPS
 
 
-def RunGame(): 
-  computer_board = Battleship([[" "] * BOARD_SIZE for i in range(BOARD_SIZE)])
-  user_guess_board = Battleship([[" "] * BOARD_SIZE for i in range(BOARD_SIZE)])
-  board, ships_location = Battleship.create_ships(computer_board)
+def RunGame():
+    computer_board = Battleship(
+        [[" "] * BOARD_SIZE for i in range(BOARD_SIZE)])
+    user_guess_board = Battleship(
+        [[" "] * BOARD_SIZE for i in range(BOARD_SIZE)])
+    board, ships_location = Battleship.create_ships(computer_board)
 
-  turns = NUMBER_GUESS
-  while turns > 0:
-    
-    #get user input
-    user_x_row, user_y_column = Battleship.get_user_input(object)
-    
-    #check if duplicate guess
-    while user_guess_board.board[user_x_row][user_y_column] == "-" or user_guess_board.board[user_x_row][user_y_column] == "X":
-      print("You guessed that one already")
-      user_x_row, user_y_column = Battleship.get_user_input(object)
-    
-    #check for hit or miss
-    if computer_board.board[user_x_row][user_y_column] == "X":
-      print("You sunk 1 of my battleship!")
-      user_guess_board.board[user_x_row][user_y_column] = "X"
-      ships_location.remove([user_x_row,user_y_column])
-    else:
-      # print("You missed my battleship!")
-      user_guess_board.board[user_x_row][user_y_column] = "-"
-      for i in create_message(user_x_row,user_y_column,ships_location):print(i)
-        
-              
-    #check for win or lose
-    if Battleship.count_hit_ships(user_guess_board) == NUMBER_SHIPS:
-      print("You hit all 5 battleships! You WIN")
-      break
-    else:
-      turns -= 1
-      print(f"You have {turns} turns remaining")
-      if turns == 0:
-        print("Sorry you ran out of turns, GAME OVER")
-        break
+    turns = NUMBER_GUESS
+    while turns > 0:
+
+        # get user input
+        user_x_row, user_y_column = Battleship.get_user_input(object)
+
+        # check if duplicate guess
+        while user_guess_board.board[user_x_row][user_y_column] == "-" or user_guess_board.board[user_x_row][user_y_column] == "X":
+            print("You guessed that one already")
+            user_x_row, user_y_column = Battleship.get_user_input(object)
+
+        # check for hit or miss
+        if computer_board.board[user_x_row][user_y_column] == "X":
+            print("You sunk 1 of my battleship!")
+            user_guess_board.board[user_x_row][user_y_column] = "X"
+            ships_location.remove([user_x_row, user_y_column])
+        else:
+            # print("You missed my battleship!")
+            user_guess_board.board[user_x_row][user_y_column] = "-"
+            for i in create_message(user_x_row, user_y_column, ships_location):
+                print(i)
+
+        # check for win or lose
+        if Battleship.count_hit_ships(user_guess_board) == NUMBER_SHIPS:
+            print("You hit all 5 battleships! You WIN")
+            break
+        else:
+            turns -= 1
+            print(f"You have {turns} turns remaining")
+            if turns == 0:
+                print("Sorry you ran out of turns, GAME OVER")
+                break
+
 
 if __name__ == '__main__':
-  RunGame()
+    RunGame()
