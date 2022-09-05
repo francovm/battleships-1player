e8557f446d116c2c1df31a9bc3fc9f91a2e75751
Franco Vega <franco.vegam@gmail.com>
2022-09-06 05:43:54 +1200

    setting codebase

---
+++ src/main.py
@@ -0,0 +1,46 @@
+
+from battleship import Battleship,create_message
+from constants import BOARD_SIZE,NUMBER_GUESS,NUMBER_SHIPS
+
+
+
+def RunGame(): 
+  computer_board = Battleship([[" "] * BOARD_SIZE for i in range(8)])
+  user_guess_board = Battleship([[" "] * BOARD_SIZE for i in range(8)])
+  board, ships_location = Battleship.create_ships(computer_board)
+
+  turns = NUMBER_GUESS
+  while turns > 0:
+    
+    #get user input
+    user_x_row, user_y_column = Battleship.get_user_input(object)
+    
+    #check if duplicate guess
+    while user_guess_board.board[user_x_row][user_y_column] == "-" or user_guess_board.board[user_x_row][user_y_column] == "X":
+      print("You guessed that one already")
+      user_x_row, user_y_column = Battleship.get_user_input(object)
+    
+    #check for hit or miss
+    if computer_board.board[user_x_row][user_y_column] == "X":
+      print("You sunk 1 of my battleship!")
+      user_guess_board.board[user_x_row][user_y_column] = "X"
+      ships_location.remove([user_x_row,user_y_column])
+    else:
+      # print("You missed my battleship!")
+      user_guess_board.board[user_x_row][user_y_column] = "-"
+      for i in create_message(user_x_row,user_y_column,ships_location):print(i)
+        
+              
+    #check for win or lose
+    if Battleship.count_hit_ships(user_guess_board) == NUMBER_SHIPS:
+      print("You hit all 5 battleships! You WIN")
+      break
+    else:
+      turns -= 1
+      print(f"You have {turns} turns remaining")
+      if turns == 0:
+        print("Sorry you ran out of turns, GAME OVER")
+        break
+
+if __name__ == '__main__':
+  RunGame()
